640e276464e4694af5953c5ac190b3659c67ae7b
Avrenak <baptiste.aurelien@gmail.com>
2020-01-31 14:11:53 +0100

    Ajout du bouton et du formulaire register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +30,6 @@ import { OrderService } from './core/services/order.service';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FontAwesomeModule,
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
